bfea247d98f947c9182fbbb4bea0054d5873d6af
Sleepless-Knights-Games <evan.d.wilcock@gmail.com>
2025-04-30 23:08:56 +0100

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -4,15 +4,15 @@ def simulate_game(initial_events, initial_blanks, max_turns, consecutive_events_
     results = [0] * (max_turns + 1)
 
     for _ in range(num_simulations):
+        revealed = []
         deck = ['E'] * initial_events + ['B'] * initial_blanks
         np.random.shuffle(deck)
 
-        revealed = []
         consecutive_events = 0
         turn = 1
 
         while turn <= max_turns and deck:
-            card = deck.pop(np.random.randint(len(deck)))
+            card = deck.pop(0)  # Draw from the top
 
             if card == 'E':
                 revealed.append(card)
@@ -20,11 +20,13 @@ def simulate_game(initial_events, initial_blanks, max_turns, consecutive_events_
                 if consecutive_events >= consecutive_events_to_end:
                     results[turn] += 1
                     break
-            else:  # Blank drawn
+            else:
+                # Blank drawn
                 consecutive_events = 0
-                # Remove blank from game, reshuffle revealed events back into deck
-                deck += revealed
+                # Remove the blank (already done), reshuffle events back in
+                deck.extend(revealed)
                 revealed = []
+                np.random.shuffle(deck)  # Shuffle after reshuffling in
 
             turn += 1
 
