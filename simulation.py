801a2fa424361ff6d46b81448d41cece4a59b827
Sleepless-Knights-Games <evan.d.wilcock@gmail.com>
2025-04-30 23:22:26 +0100

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,44 +1,32 @@
 import numpy as np
 
-def simulate_game_corrected(initial_events, initial_blanks, max_turns, consecutive_events_to_end, num_simulations=50000):
-    results = [0] * (max_turns + 1)
-
-    for _ in range(num_simulations):
-        # Initialise the deck
+def simulate_game(initial_events, initial_blanks, max_turns, consecutive_events_to_end, simulations=10000):
+    results = np.zeros(max_turns)
+    
+    for _ in range(simulations):
         deck = ['E'] * initial_events + ['B'] * initial_blanks
-        np.random.shuffle(deck)
         revealed = []
+        turn = 0
+        consecutive = 0
 
-        consecutive_events = 0
-        turn = 1
-
-        while turn <= max_turns and deck:
-            card = deck.pop(0)
-
-            if card == 'E':
-                revealed.append('E')
-                consecutive_events += 1
-                if consecutive_events >= consecutive_events_to_end:
-                    results[turn] += 1
-                    break
-            else:
-                # Blank resets consecutive counter
-                consecutive_events = 0
-                # Return revealed events to the deck and reshuffle
-                deck.extend(revealed)
-                np.random.shuffle(deck)
-                revealed = []
+        for t in range(max_turns):
+            if not deck:
+                break
 
             turn += 1
+            drawn = np.random.choice(deck)
+            deck.remove(drawn)
 
-        # If we reached the end without triggering, record nothing (will count in total)
+            if drawn == 'E':
+                revealed.append('E')
+                consecutive += 1
+                if consecutive >= consecutive_events_to_end:
+                    results[turn - 1] += 1
+                    break
+            else:
+                deck += revealed
+                revealed = []
+                consecutive = 0
 
-    total = sum(results)
-    cumulative = 0
-    prob_by_turn = []
-    for i in range(1, max_turns + 1):
-        cumulative += results[i]
-        prob = cumulative / total if total else 0
-        prob_by_turn.append((i, prob))
-
-    return prob_by_turn
+    cumulative = np.cumsum(results) / simulations
+    return list(zip(range(1, max_turns + 1), cumulative))
